d43dd4e6023a5c724b66bd3848ed46e1734d025a
Andrew Short <andrewshort@Andrews-MacBook-Pro.local>
2021-03-06 00:44:06 -0800

    add episode directory if not already exists

--- build.js
+++ build.js
@@ -1,14 +1,16 @@
-const { readFileSync, readdirSync } = require('fs');
+const { readFileSync, readdirSync, mkdirSync } = require('fs');
 const { spawn } = require('child_process');
 
-const ls = readdirSync('./temp');
+const lsTmp = readdirSync('./temp');
+const lsAudio = readdirSync('./audio');
 
-let playlist = Array(ls.length);
+let playlist = Array(lsTmp.length);
 
-for (let i = 0; i < ls.length; i += 1) {
-  let path = './temp/' + ls[i];
+for (let i = 0; i < lsTmp.length; i += 1) {
+
+  let path = './temp/' + lsTmp[i];
   let { playlist_index, id } = JSON.parse(readFileSync(path));
-  playlist[playlist_index - 1] = path;
-}
+  playlist[playlist_index - 1] = id;
 
-console.log(playlist);
+  if (!lsAudio.includes(id)) { mkdirSync('./audio/' + id); }
+}
