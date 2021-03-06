af1d1b3d9d4bc755605b563a97a1176f1c9a085e
Andrew Short <andrewshort@Andrews-MacBook-Pro.local>
2021-03-06 03:41:59 -0800

    write format date function

--- build.js
+++ build.js
@@ -1,3 +1,7 @@
+const { formatDate } = require('./format_date.js');
+
 const Handlebars = require("handlebars");
 const template = Handlebars.compile("Name: {{name}}");
 console.log(template({ name: "Nils" }));
+
+console.log(formatDate());
---
+++ format_date.js
@@ -0,0 +1,47 @@
+const months = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec'
+];
+
+const days = [
+  'Sun',
+  'Mon',
+  'Tue',
+  'Wed',
+  'Thu',
+  'Fri',
+  'Sat'
+];
+
+function twoDigits(n) {
+  let s = n.toString();
+  if (s.length < 2) {
+    return '0' + s;
+  }
+  return s;
+}
+
+const date = new Date();
+
+function formatDate() {
+  return days[date.getDay()] + ', ' +
+    date.getDay() + ' ' +
+    months[date.getMonth()] + ' ' +
+    date.getFullYear() + ' ' +
+    twoDigits(date.getHours()) + ':' +
+    twoDigits(date.getMinutes()) + ':' +
+    twoDigits(date.getSeconds()) + ' ' +
+    'EST';
+}
+
+module.exports = { formatDate };
