70959e8caeb65b22103b64e79e011c5799dd81bb
Andrew Short <andrewshort@Andrews-MacBook-Pro.local>
2021-03-06 01:57:15 -0800

    downloading audio files

--- build.js
+++ build.js
@@ -1,5 +1,5 @@
 const { readFileSync, readdirSync, mkdirSync } = require('fs');
-const { spawn } = require('child_process');
+const { execSync } = require('child_process');
 
 const lsTmp = readdirSync('./temp');
 const lsAudio = readdirSync('./audio');
@@ -9,8 +9,21 @@ let playlist = Array(lsTmp.length);
 for (let i = 0; i < lsTmp.length; i += 1) {
 
   let path = './temp/' + lsTmp[i];
-  let { playlist_index, id } = JSON.parse(readFileSync(path));
+  let { playlist_index, id, title } = JSON.parse(readFileSync(path));
   playlist[playlist_index - 1] = id;
 
-  if (!lsAudio.includes(id)) { mkdirSync('./audio/' + id); }
+
+  if (!lsAudio.includes(id)) {
+    mkdirSync('./audio/' + id);
+    console.log('DOWNLOADING ' + title);
+    execSync(
+      'youtube-dl --extract-audio ' +
+      id +
+      ' -o "./audio/%(id)s/episode.%(ext)s"' +
+      ' --audio-format m4a'
+    );
+    console.log('...FINISHED');
+    // console.log('copying metadata');
+    // execSync('cp ' + path + ' ./audio/' + id + '/metadata.json');
+  }
 }
--- fetch_data.sh
+++ fetch_data.sh
@@ -1,4 +1,5 @@
 rm -r temp ;
 mkdir temp ;
+mkdir audio ;
 cd temp &&
 youtube-dl --write-info-json --skip-download UUNLORxbPEwf98GlydIahIEg
